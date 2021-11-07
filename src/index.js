c4a92d7580b740008c895d551501f648bd880761
bryanpascuadizon <dizon_danielle@yahoo.com>
2021-11-07 17:22:10 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+//Components
+import Main from './Main'
+
+ReactDOM.render(
+    <Fragment>
+        <Main />
+    </Fragment>
+    , document.getElementById('root'))
+
+//reportWebVitals(console.log)
+
